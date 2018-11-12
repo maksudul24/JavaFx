712ac8b7cafab7bac82fa57bf084677fad951bb3
maksudul24 <maksudulrabat@gmail.com>
2018-11-13 01:32:04 +0600

    Works with Decimal points

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 public class MainController {
 	@FXML
 	private Label result;
-	private long number1=0;
+	private float number1=0;
 	private String operator="";
 	private boolean start= true;
 	private Model model = new Model();
@@ -33,8 +33,8 @@ public class MainController {
 			if(!operator.isEmpty()) return;
 			operator=value;
 			sw=true;
-			number1 = Long.parseLong(result.getText());
-		/*	temp= new String(result.getText());
+		//	number1 = Long.parseLong(result.getText());
+			temp= new String(result.getText());
 			number1=0;
 			dec=(float) 0.1;
 			for(int i=0;i<temp.length();i++) {
@@ -51,14 +51,14 @@ public class MainController {
 					dec=(float)(dec/10);
 					
 				}
-			} */
+			} 
 			
 			result.setText("");
 		}
 		else {
 			if(operator.isEmpty()) return;
-			long number2=Long.parseLong(result.getText());
-		/*	float number2=0;
+		//	long number2=Long.parseLong(result.getText());
+			float number2=0;
 			temp= new String(result.getText());
 			dec=(float) 0.1;
 			for(int i=0;i<temp.length();i++) {
@@ -75,7 +75,7 @@ public class MainController {
 					dec=(float)(dec/10);
 					
 				}
-			} */
+			} 
 			float output = model.calculate(number1, number2, value);
 			result.setText(String.valueOf(output));
 		}
